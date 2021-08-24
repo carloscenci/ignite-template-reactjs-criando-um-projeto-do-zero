40c760892d1a20b7cb5b398385c510f1d715b20b
carloscenci <carlosc.estevam@gmail.com>
2021-08-23 23:21:50 -0300

    Styling post page

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,4 +1,6 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
+import { FiCalendar, FiClock, FiUser } from 'react-icons/fi'
+import Header from '../../components/Header';
 
 import { getPrismicClient } from '../../services/prismic';
 
@@ -26,9 +28,43 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post({ post }: PostProps) {
+  return (
+    <>
+      <Header />
+      <img src="/images/avatar.svg" alt="imagem" className={styles.banner} />
+      <main className={commonStyles.container}>
+        <div className={styles.post}>
+          <div className={styles.postTop}>
+            <h1>Algum titulo</h1>
+            <ul>
+              <li>
+                <FiCalendar />
+                {/* {formatedDate} */}
+                12 Mar 2021
+              </li>
+              <li>
+                <FiUser />
+                {/* {post.data.author} */}
+                Carlos Cenci
+              </li>
+              <li>
+                <FiClock />
+                {/* {`${readTime} min`} */}
+                5 min
+              </li>
+            </ul>
+          </div>
+
+          <article>
+            <h2>Titulo seção</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, <strong>adipisicing elit</strong>. Optio expedita <a href="#"></a>, natus vitae facere architecto nesciunt. Maiores provident quo at ipsum iste obcaecati repellendus quasi nisi.</p>
+          </article>
+        </div>
+      </main>
+    </>
+  )
+}
 
 // export const getStaticPaths = async () => {
 //   const prismic = getPrismicClient();
